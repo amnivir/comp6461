5602a1a649538fff186e2a6c246df8b1b950b070
Shivam <shivam.nigam@ericsson.com>
2016-12-04 11:41:57 -0500

    Add flow monitor & tcp FTP server

--- Project.cc
+++ Project.cc
@@ -11,100 +11,124 @@
 #include "ns3/applications-module.h"
 #include "ns3/csma-module.h"
 #include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/delay-jitter-estimation.h"
+#include "ns3/flow-monitor.h"
+#include "ns3/flow-monitor-helper.h"
 
 using namespace ns3;
 
 
 NS_LOG_COMPONENT_DEFINE ("FTP_Sim");
-
+/**
+ * Topology
+ *
+ * Node 1 (Client) ----CSMA--- Node 2 (Server)
+ */
 int main(int argc, char *argv[])
 {
 
-    Time::SetResolution (Time::NS);
-    LogComponentEnable ("OnOffApplication", LOG_LEVEL_INFO);
-    LogComponentEnable ("OnOffApplication", LOG_LEVEL_INFO);
+    LogComponentEnable ("BulkSendApplication", LOG_LEVEL_INFO);
+    //bool tracing = true;
+    uint32_t maxBytes = 0;
+    //
+    // Allow the user to override any of the defaults at
+    // run-time, via command-line arguments
+    //
+    /*CommandLine cmd;
+    cmd.AddValue ("tracing", "Flag to enable/disable tracing", tracing);
+    cmd.AddValue ("maxBytes",
+            "Total number of bytes for application to send", maxBytes);
+    cmd.Parse (argc, argv);
+*/
+    //
+    // Explicitly create the nodes required by the topology (shown above).
+    //
+    NS_LOG_INFO ("Create nodes.");
+    NodeContainer nodes;
+    nodes.Create (2);
 
+    NS_LOG_INFO ("Create channels.");
 
-    NodeContainer csmaNodes;
+    //
+    // Explicitly create the point-to-point link required by the topology (shown above).
+    //
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("500Kbps"));
+    pointToPoint.SetChannelAttribute ("Delay", StringValue ("5ms"));
 
-    csmaNodes.Create(2);
+    NetDeviceContainer devices;
+    devices = pointToPoint.Install (nodes);
 
+    //
+    // Install the internet stack on the nodes
+    //
+    InternetStackHelper internet;
+    internet.Install (nodes);
 
-    // using csma helper to configure attributes
-    // and creating connectivity between two nodes
-    CsmaHelper csma;
+    //
+    // We've got the "hardware" in place.  Now we need to add IP addresses.
+    //
+    NS_LOG_INFO ("Assign IP Addresses.");
+    Ipv4AddressHelper ipv4;
+    ipv4.SetBase ("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer i = ipv4.Assign (devices);
 
-    // set channel attributes for device
-    csma.SetChannelAttribute ("DataRate",StringValue("1Mbps"));
-    csma.SetChannelAttribute ("Delay",StringValue("20ms"));
+    NS_LOG_INFO ("Create Applications.");
 
+    //
+    // Create a BulkSendApplication and install it on node 0
+    //
+    uint16_t port = 9;  // well-known echo port number
 
+    //send to node 1
+    BulkSendHelper source ("ns3::TcpSocketFactory",
+            InetSocketAddress (i.GetAddress (1), port));
+    // Set the amount of data to send in bytes.  Zero is unlimited.
+    source.SetAttribute ("MaxBytes", UintegerValue (maxBytes));
+    ApplicationContainer sourceApps = source.Install (nodes.Get (0));
+    sourceApps.Start (Seconds (0.0));
+    sourceApps.Stop (Seconds (10.0));
 
-    // installing nodes to create, configure, install devices of required type
-    NetDeviceContainer csmaDevices;
+    //
+    // Create a PacketSinkApplication and install it on node 1
+    //
+    PacketSinkHelper sink ("ns3::TcpSocketFactory",
+            InetSocketAddress (Ipv4Address::GetAny (), port));
+    ApplicationContainer sinkApps = sink.Install (nodes.Get (1));
+    sinkApps.Start (Seconds (0.0));
+    sinkApps.Stop (Seconds (10.0));
 
-    csmaDevices = csma.Install(csmaNodes);
+    //
+    // Set up tracing if enabled
+    //
+/*
+//    if (tracing)
+    {
+        AsciiTraceHelper ascii;
+        pointToPoint.EnableAsciiAll (ascii.CreateFileStream ("tcp-bulk-send.tr"));
+        pointToPoint.EnablePcapAll ("tcp-bulk-send", true);
+    }
+*/
+//collect reults
+    // Flow monitor
+    Ptr<FlowMonitor> flowMonitor;
+    FlowMonitorHelper flowHelper;
+    flowMonitor = flowHelper.InstallAll();
 
-    // installing protocol stack in nodes; once executed will install TCP,UDP, IP etc. on each node of the nodeContainer
-
-    InternetStackHelper stack;
-    stack.Install(csmaNodes);
-
-    // assigning IP address to the devices, setting base address here
-    Ipv4AddressHelper address;
-    address.SetBase ("10.10.10.0","255.255.255.0");
-
-
-    Ipv4InterfaceContainer csmaInterfaces =address.Assign(csmaDevices);
-    // assigning ip address to the devices, since devices contain nodes, while ip stack is created within a node
-
-
-
-
-    //  unint32_t port_num=21; // port number for Rx
-
-
-    Address RxAddress(InetSocketAddress (csmaInterfaces.GetAddress(1), 10)); //Rx obtaining a IPv4 address, and the port number to listen
-
-    PacketSinkHelper sinkHelper ("ns3::TcpSocketFactory", RxAddress); // PacketSinkHelper is a class reference for creating socket references and passing address at which Rx is instantiated
-
-    ApplicationContainer sinkApp = sinkHelper.Install (csmaNodes.Get(1)); // ApplicationContainer, installs application on the node, in our case it is packet sink helper ; node here is the server node
-
-    sinkApp.Start (Seconds (1.0));
-
-    sinkApp.Stop (Seconds (10.0));
-
-
-
-    //creating a TCP transmitter
-    // set up address and port number to  send TCP traffic to the server
-    Address TxAddress(InetSocketAddress(csmaInterfaces.GetAddress(1),10));
-
-    OnOffHelper clientHelper ("ns3::TcpSocketFactory", TxAddress);
-
-    // setting up attributes for onoff application helper
-    clientHelper.SetAttribute("DataRate",StringValue("1Mbps"));
-    clientHelper.SetAttribute("PacketSize",UintegerValue(1280));
-    ApplicationContainer Tx = clientHelper.Install (csmaNodes.Get (0));
-
-
-    csma.EnablePcap("trial",csmaDevices.Get(1),true);
-    Tx.Start (Seconds (1.0));
-    Tx.Stop (Seconds (10.0));
-
-
-
+    //
+    // Now, do the actual simulation.
+    //
     NS_LOG_INFO ("Run Simulation.");
+    Simulator::Stop (Seconds (10.0));
     Simulator::Run ();
-    NS_LOG_INFO ("Done");
+    Simulator::Destroy ();
+    NS_LOG_INFO ("Done.");
 
+    flowMonitor->SerializeToXmlFile("ftp_test.xml", true, true);
 
+    Ptr<PacketSink> sink1 = DynamicCast<PacketSink> (sinkApps.Get (0));
+    std::cout << "Total Bytes Received at client: " << sink1->GetTotalRx () << std::endl;
 
-
-
-
-
-    return(0);
 }
 
 
