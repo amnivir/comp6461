508b19ea0fa5f50c10aa1da8ad988e32470e2831
Shivam <shivam.nigam@ericsson.com>
2016-12-04 21:34:22 -0500

    Fix Delay: average delay for all packets

--- Project.cc
+++ Project.cc
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
     // Explicitly create the point-to-point link required by the topology (shown above).
     //
     PointToPointHelper pointToPoint;
-    pointToPoint.SetDeviceAttribute("DataRate", StringValue("500Kbps"));
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("1Mbps"));
     pointToPoint.SetChannelAttribute("Delay", StringValue("5ms"));
 
     NetDeviceContainer devices;
@@ -83,7 +83,7 @@ int main(int argc, char *argv[])
     uint16_t port = 9;  // well-known echo port number
 
     //send to node 1
-    std::cout<<"FTP_SERVER_IP"<<i.GetAddress(0)<<"\n";
+    std::cout<<"FTP_SERVER_IP: "<<i.GetAddress(0)<<"\n";
     BulkSendHelper source("ns3::TcpSocketFactory", InetSocketAddress(i.GetAddress(1), port));
     // Set the amount of data to send in bytes.  Zero is unlimited.
     source.SetAttribute("MaxBytes", UintegerValue(maxBytes));
@@ -94,7 +94,7 @@ int main(int argc, char *argv[])
     //
     // Create a PacketSinkApplication and install it on node 1
     //
-    std::cout<<"CLIENT_IP"<<i.GetAddress(1)<<"\n";
+    std::cout<<"CLIENT_IP: "<<i.GetAddress(1)<<"\n";
     PacketSinkHelper sink("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port));
     ApplicationContainer sinkApps = sink.Install(nodes.Get(1));
     sinkApps.Start(Seconds(0.0));
@@ -147,8 +147,8 @@ int main(int argc, char *argv[])
         std::cout << "  Throughput: "
                 << i->second.rxBytes * 8.0
                 / (i->second.timeLastRxPacket.GetSeconds()
-                        - i->second.timeFirstTxPacket.GetSeconds()) / 1024 << " Kbps";
-        std::cout << "  Delay:      " << i->second.delaySum / 1000 / 1000 / 1000 << " Sec.\n";
+                        - i->second.timeFirstTxPacket.GetSeconds()) / 1024 << " Kbps \n";
+        std::cout << "  Delay:      " << i->second.delaySum / i->second.rxPackets  <<"\n";
     }
     monitor->SerializeToXmlFile("Project6461.xml", true, true);
     Simulator::Destroy();
